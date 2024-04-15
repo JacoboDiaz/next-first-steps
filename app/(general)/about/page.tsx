509710f081cdb49ee75abbc966b7f2e6b1b89cd6
jacobodiaz <jacobo.diaz@hotmail.com>
2024-04-15 16:54:46 -0600

    first commit

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+ title: 'About',
+ description: 'About',
+};
+
+export default function AboutPage(){
+    return (
+        <>
+            <span className="text-7xl">About Page</span>
+        </>
+    );
+}
